d61273630d33f173a1c5f5ab1728a3e1602de5a3
raulnabarret <raulnabarret@gmail.com>
2016-04-13 23:12:47 -0500

    Dibujar y mover a Tifis en el canvas

--- Legend of Tifis/tifis.js
+++ Legend of Tifis/tifis.js
@@ -1,24 +1,58 @@
 var tablero;
 var fondo = {
-	imagenURL: "media/fondo.png"
+	imagenURL: "media/fondo.png",
+	imagenOK : false,
 };
 
 var tifis = {
-	
+	x : 100,
+	y : 100,
+	frenteURL: "media/diana-frente.png",
+	frenteOK: false
 };
 
-function inicio () {
+function inicio() {
 
 	var canvas = document.getElementById("campo");
 	tablero = canvas.getContext("2d");
 
 	fondo.imagen = new Image();
 	fondo.imagen.src = fondo.imagenURL;
+	fondo.imagen.onload = confirmarFondo;
 
-	fondo.imagen.onload = dibujar;
+	tifis.frente = new Image();
+	tifis.frente.src = tifis.frenteURL;
+	tifis.frente.onload = confirmarFrente;
+
+	var m = document.getElementById("mover");
+	m.addEventListener("click", movimiento);
 } 
 
-function dibujar () {
+function confirmarFondo() {
 
-	tablero.drawImage(fondo.imagen, 0, 0);
+	fondo.imagenOK = true;
+	dibujar();
+}
+
+function confirmarFrente() {
+
+	tifis.frenteOK = true;
+	dibujar();
+}
+
+function dibujar() {
+
+	if (fondo.imagenOK == true) { 
+		tablero.drawImage(fondo.imagen, 0, 0);
+	}
+	if (tifis.frenteOK == true) {
+		tablero.drawImage(tifis.frente, tifis.x, tifis.y);
+	}
+}
+
+function movimiento() {
+
+	tifis.x += 10;
+	dibujar();
+
 }
