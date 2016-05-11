9e78420d11f09b591d4855c7dbc7fe1249ae1709
raulnabarret <raulnabarret@gmail.com>
2016-05-11 18:55:57 -0500

    Envío de formulario a través de Node.js usando el método POST

--- Node/backend.js
+++ Node/backend.js
@@ -1,9 +1,19 @@
 var express = require("express");
+var parse = require("body-parser");
 var web = express();
+web.use( parse.urlencoded() );
 var servidor;
 
 servidor = web.listen("8080", function () {
 	console.log("HTTP 200");
+});
+
+web.get("/", function(req, res) {
+	res.sendfile("index.html");
+});
+
+web.post("/user", function(req, res) {
+	res.send("Now you are logged in");
 });
 
 web.get("/about", function(req, res) {
