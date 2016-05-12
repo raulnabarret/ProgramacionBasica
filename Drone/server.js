989a2a5c2dda202917239959adf9272f930fe813
raulnabarret <raulnabarret@gmail.com>
2016-05-12 10:14:13 -0500

    Controlar Drone (Despegue/Rotación/Aterrizaje)

--- Drone/server.js
+++ Drone/server.js
@@ -1,7 +1,30 @@
 var arDrone = require("ar-drone");
+var myDrone = arDrone.createClient();
 
 var express = require("express");
 var web = express();
+
+
+function battery () {
+	console.log("Battery is " + myDrone.battery());
+}
+
+function takeoff () {
+	myDrone.config("control:altitude_max", 100000);
+	myDrone.takeoff();
+	rotate();
+}
+
+function rotate () {
+	myDrone.stop();
+	myDrone.calibrate(0);
+}
+
+function landing () {
+	myDrone.stop();
+	myDrone.land();
+
+}
 
 servidor = web.listen(8080, function () {
 	console.log("HTTP 200");
@@ -9,14 +32,19 @@ servidor = web.listen(8080, function () {
 
 web.get("/", function (req, res){
 	res.sendfile("drone.html");
+	battery();
 });
 
 web.get("/despegar", function (req, res){
 	console.log("Despegar");
+	battery();
+	takeoff();
 	res.sendfile("drone.html");
 });
 
 web.get("/aterrizar", function (req, res){
 	console.log("Aterrizar");
+	battery();
+	landing();
 	res.sendfile("drone.html");
 });
